918684fd4eaff1b50b1aa1533d0082f115fd18e7
niravpatel-iitm <niravpatel@smail.iitm.ac.in>
2026-07-29 16:20:31 +0530

    Delete src/demos/ur5_control/launch/ur5_control.launch.py

--- src/demos/ur5_control/launch/ur5_control.launch.py
+++
@@ -1,7 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='ur5_control', executable='joint_controller', name='joint_controller'),
-    ])
