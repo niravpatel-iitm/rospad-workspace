0ad9eb91346645725fe1d84329f14e3505a57d77
niravpatel-iitm <niravpatel@smail.iitm.ac.in>
2026-07-29 16:42:41 +0530

    Update src/demos/vision_demos/launch/color_tracker.launch.py

---
+++ src/demos/vision_demos/launch/color_tracker.launch.py
@@ -0,0 +1,7 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(package='vision_demos', executable='color_tracker', name='color_tracker'),
+    ])
